54e3efbbdf3a930bb1a191e4d21460382f1c25f1
Ekaterina <yatakshilina@gmail.com>
2023-06-30 10:57:23 -0400

    add calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -4,7 +4,21 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 
-puts "Exercise 5"
-puts "----------"
+puts 'Exercise 5'
+puts '----------'
 
 # Your code goes here ...
+### Exercise 5: Calculations
+
+# 1. Output the total revenue for the entire company (all stores), using Active Record's `.sum` calculation method.
+# 2. On the next line, also output the average annual revenue for all stores.
+# 3. Output the number of stores that are generating $1M or more in annual sales. **Hint:** Chain together `where` and `size` (or `count`) Active Record methods.
+
+total_revenue = Store.sum(:annual_revenue)
+puts "Total Company Revenue: $#{total_revenue}"
+
+average_revenue = Store.average(:annual_revenue)
+puts "Average Company Revenue: $#{average_revenue}"
+
+number_of_stores_generating_one_million_or_more_annual_sales = Store.where('annual_revenue > ?', 1_000_000).count
+puts "The number of stores that are generating $1M or more in annual sales #{number_of_stores_generating_one_million_or_more_annual_sales}"
